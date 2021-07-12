398864b6882c1dd1de67bde5e5a3ceaa1bbb39c9
saikrishnamylaram <75681251+saikrishnamylaram@users.noreply.github.com>
2021-07-12 21:34:55 +0530

    Update common_seed.groovy
    done

--- jenkins/common_seed.groovy
+++ jenkins/common_seed.groovy
@@ -1,36 +1,37 @@
-def baseUrl = "https://github.com/saikrishnamylaram/"
+def baseUrl = "https://github.com/ybmadhu/"
 def repoName = "$reponame"
 def gitRepoUrl= baseUrl + repoName + '.git'
 def jobName = "$reponame"
-  pipelineJob(jobName) {
-        properties {
-         pipelineTriggers {
-          triggers {
-            pollSCM {
-              scmpoll_spec('*/1 * * * *')
-              ignorePostCommitHooks(true)
-            }
-          }
-         }
-        }
-        logRotator{
-         numToKeep(5)
-        }
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        remote {
-                            url(gitRepoUrl)
-                            credentials('git_credentials')
-                        }
-                        branches('master')
-                        extensions {
-                            cleanBeforeCheckout()
-                        }
-                    }
-                }
-                scriptPath("jenkinsfile")
-            }
-        }
-    }
+ pipelineJob(jobName) {
+ properties {
+ pipelineTriggers {
+ triggers {
+ pollSCM {
+ scmpoll_spec('*/1 * * * *')
+ ignorePostCommitHooks(true)
+ }
+ }
+ }
+ }
+ logRotator{
+ numToKeep(5)
+ }
+ definition {
+ cpsScm {
+ scm {
+ git {
+ remote {
+ url(gitRepoUrl)
+ credentials('github_credentials')
+ }
+ branches('master')
+ extensions {
+ cleanBeforeCheckout()
+ }
+ }
+ }
+ scriptPath("Jenkinsfile")
+ }
+ }
+ }
+
