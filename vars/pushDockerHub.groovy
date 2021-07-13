8ba1022aa78566415f987e7ffaa10d3457c12c6d
saikrishnamylaram <75681251+saikrishnamylaram@users.noreply.github.com>
2021-07-13 19:56:05 +0530

    Update pushDockerHub.groovy
    changes done

--- vars/pushDockerHub.groovy
+++ vars/pushDockerHub.groovy
@@ -4,7 +4,7 @@ def projectName = pipelineParams.ecrRepoName
 pipeline {
  agent any
   environment {
-    registry = "ybmsr/${projectName}"
+    registry = "saikrishnamsk/${projectName}"
     registryCredential = 'dockerhub_credentials'
     dockerImage = ''
   }
