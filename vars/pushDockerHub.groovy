6c386ccb60cc3ce185081604dd5cb799f1e4ea68
saikrishnamylaram <75681251+saikrishnamylaram@users.noreply.github.com>
2021-07-14 20:37:52 +0530

    Update pushDockerHub.groovy

--- vars/pushDockerHub.groovy
+++ vars/pushDockerHub.groovy
@@ -1,35 +1,34 @@
 def call(Map pipelineParams){
 def projectName = pipelineParams.ecrRepoName
-
 pipeline {
- agent any
-  environment {
-    registry = "saikrishnamsk/${projectName}"
-    registryCredential = 'dockerhub_credentials'
-    dockerImage = ''
-  }
-  stages {
-    stage('Building image') {
-      steps{
-        script {
-          dockerImage = docker.build registry + ":$BUILD_NUMBER"
-        }
-      }
-    }
-    stage('push image') {
-      steps{
-        script {
-          docker.withRegistry( '', registryCredential ) {
-            dockerImage.push()
-          }
-        }
-      }
-    }
-    stage('Remove old docker image') {
-      steps{
-        sh "docker rmi $registry:$BUILD_NUMBER"
-      }
-    }
-  }
+agent any
+ environment {
+ registry = "saikrishnamsk/${projectName}"
+ registryCredential = 'dockerhub_credentials'
+ dockerImage = ''
+ }
+ stages {
+ stage('Building image') {
+ steps{
+ script {
+ dockerImage = docker.build registry + ":$BUILD_NUMBER"
+ }
+ }
+ }
+ stage('push image') {
+ steps{
+ script {
+ docker.withRegistry( '', registryCredential ) {
+ dockerImage.push()
+ }
+ }
+ }
+ }
+ stage('Remove old docker image') {
+ steps{
+ sh "docker rmi $registry:$BUILD_NUMBER"
+ }
+ }
+ }
 }
 }
